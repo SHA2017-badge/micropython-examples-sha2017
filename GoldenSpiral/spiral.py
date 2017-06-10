fcd7e74839abde09371569d572775598d00c1886
Anne Jan Brouwer <brouwer@annejan.com>
2017-06-10 07:53:39 +0200

    Cleanup :)

--- GoldenSpiral/spiral.py
+++ GoldenSpiral/spiral.py
@@ -1,3 +1,5 @@
+import ugfx
+ugfx.init()
 # adapted from https://codegolf.stackexchange.com/a/39983
 from math import *
 n=int(10)
@@ -5,10 +7,6 @@ G=(1.0+6.0**.5)/2.0
 w=int(G**(4.0*(n//4.0)))
 k=pi/90
 twist = 100
-import badge
-badge.init()
-import ugfx
-ugfx.init()
 ugfx.clear(ugfx.WHITE)
 for j in range(n*twist):
     ugfx.pixel(int(G**(j/float(twist))*cos(j*k)+w/2), int(G**(j/float(twist))*sin(j*k)+w/2.0), ugfx.BLACK)
